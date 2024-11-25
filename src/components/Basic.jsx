303c6495f46fa993b5d0ded94d578868022880f2
Ahmad Ali <xevenahmad@gmail.com>
2024-11-25 13:20:03 +0500

    introduce minii CSS library for browser that cancels their own default styles and add custom styles

---
+++ src/components/Basic.jsx
@@ -0,0 +1,21 @@
+
+// Custom styles
+
+function Basic() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React application with Normalize.css applied.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      <input type="text" placeholder="Enter text" />
+      <button>Ahmad</button>
+    <button>Test</button>
+    </div>
+  );
+}
+
+export default Basic;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+import 'normalize.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import '../src/assets/styles/mycustom.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
